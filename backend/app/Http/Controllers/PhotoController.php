b7bfa2051e8603312b9c7867bb08a538d8afadb1
Matilde Wittrup Enevoldsen <matildeene@outlook.com>
2019-04-18 12:39:25 +0200

    Init project.
    Backend changes:
    
    - Add sqlite database
    - Add database migration.
    - Create photo model and controller.
    - Create routes for photo upload.
    - Create function in controller for photo upload.
    
    Frontend changes:
    
    - Init project.

---
+++ backend/app/Http/Controllers/PhotoController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Photo;
+use Illuminate\Http\Request;
+
+class PhotoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Photo  $photo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Photo $photo)
+    {
+        //
+    }
+}
---
+++ backend/routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/photos', 'PhotoController@index')->name('photos.index');
+//Route::get('/photos/data/json', 'PhotoController@json')->name('photos.json');
+Route::post('/photos', 'PhotoController@store')->name('photos.store');
+Route::get('/photos/{photo}', 'PhotoController@show')->name('photos.show');
